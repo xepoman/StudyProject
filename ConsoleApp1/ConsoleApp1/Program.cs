974eb2c996a55abcb7f5d2ff92eb313fea4046b9
xepoman <ivan.egov@mail.ru>
2019-03-02 19:42:50 +0700

    Урок 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,63 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*
-  Команда Big-dipper — это Денис, Адиль и Надира. Но это никак не помогает решить задачу.
 
-Входные данные
-Два целых числа A и B (оба от 1 до 1000).
-
-Выходные данные
-Одно целое число.
-
-    входное 
-    7 2
-    выход 
-    95
-
-    вход
-     2 2
-     выход
-     40
-
-    вход 
-    235 152
-    выход
-    38783
-
-    вход 
-    15 25
-    выход
-    0
-
-  
-  */
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            #region urok1
 
-            int a = 235;
-            int b = 152;
-            int c = 0;
+            #region urok2
+            /// <summary>
+            ///  создать часть скрипта что бы при по вышении уровня  до 100 повышались параметры 
+            /// </summary>
 
-            if ((a - b) >= 0)
+            int HPcastl = 1000; // хп здания
+            int protection = 1; // защита здания
+            int lvl; // уровень здания
+
+            int costUp = 10; // цена улутшения
+
+            int money = 10000; //заменить из глобал базы 
+            int[] lvlUp = new int[100];
+            Console.Write("Ввидите lvl: ");
+            lvl = Convert.ToInt32(Console.ReadLine());
+            
+            if(lvl > 100 )
+            {
+                lvl = 100;
+                Console.WriteLine("Уровень привышен и стал 100");
+            }
+            lvlUp[lvl] = lvl;
+            if (money >= costUp && lvl < 100)
             {
 
-                Console.WriteLine((a + b) + "" + (a - b));
+                lvl++;
+                HPcastl *= lvl;
+                protection += 3 * lvl;
+                costUp += 200 * lvl;
+                money -= costUp;
+                Console.WriteLine($"лвл : {lvl} Зашита: {protection} Деньги осталось: {money} Хп здания {HPcastl}");
             }
-            else
-            {
-                Console.WriteLine(c);
-            }
-            Console.ReadLine();
-            #endregion urok1
+
+            Console.ReadKey();
+
+            #endregion urok2
+
+
         }
+        
 
-
-
-    }
+    }   
 }
