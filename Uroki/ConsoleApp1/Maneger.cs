fcf787ce5e62f16c3d0065ac37cffdf13e3ccabf
xepoman <ivan.egov@mail.ru>
2019-03-21 17:17:04 +0700

    заменил масив колекцией

--- Uroki/ConsoleApp1/Maneger.cs
+++ Uroki/ConsoleApp1/Maneger.cs
@@ -3,41 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace ConsoleApp1
 {
     class Maneger : Emploee
     {
-        string[] emploees = new string[1];
-        int lastNull = 0;
+        List<string> emploees = new List<string>();
+      //  string[] emploees = new string[1];
+      //  int lastNull = 0;
+        int cell_number = 0;
         public Maneger(string name, string surname, int age)
             : base(name, surname, age)
         {
 
         }
-
         public void Add(string data)
         {
-            if (emploees[emploees.Length-1] != null)
+            /* if (emploees[emploees.Count-1] != null)
+             {
+                 this.lastNull = emploees.Count;
+                 string[] temp_emploees = new string[emploees.Length+5];
+                 for (int t = 0; t < this.emploees.Length; t++)
+                 {
+                     temp_emploees[t] = this.emploees[t];
+                 }
+                 this.emploees = temp_emploees;               
+             }
+             this.emploees[lastNull] = data;
+             lastNull++;*/
+            
+            emploees.Add(data);
+        }
+         int Smash() // поиск на название менеджера
+        {
+            for (int i = emploees.Count - 1; 0 <= i; i--)
             {
-                this.lastNull = emploees.Length;
-                string[] temp_emploees = new string[emploees.Length+5];
-                for (int t = 0; t < this.emploees.Length; t++)
+                string[] word = emploees[i].Split(' ');
+                for (int t = 0; t < word.Length; t++)
                 {
-                    temp_emploees[t] = this.emploees[t];
+                    if (word[t] == "Maneger")
+                    {
+                        return this.cell_number = i;
+                    }
+                    else if (word[t] == "Emploee")
+                    {
+                        return this.cell_number = i-1;
+                    }
                 }
-                this.emploees = temp_emploees;               
             }
-            this.emploees[lastNull] = data;
-            lastNull++;
+            return cell_number;
         }
         public override string GetData()
         {
-            string output = base.GetData()+ "\n";
-            for (int i=0;i<this.lastNull;i++)
-            {
-                output += "\t-- "+this.emploees[i]+"\n"; 
-            }
+            string output = base.GetData() + " \n";
+                for (int i = 0; i < emploees.Count; i++) // сортировка
+                {
+                        string temp = this.emploees[i];
+                        this.emploees[i] = this.emploees[Smash()];
+                        this.emploees[cell_number] = temp;
+                        output += "\t-- " + this.emploees[i] + "\n";
+                }
             return output;
         }
     }
--- Uroki/ConsoleApp1/Program.cs
+++ Uroki/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 /// <summary>
 /// Создать иирархию менеджер работник 
 /// Maneger M1 1000
@@ -17,18 +18,22 @@ namespace ConsoleApp1
 {
     class Program
     {
+         
 
         static public void Main(string[] args)
         {
             Maneger M1 = new Maneger("Igor", "ivanov", 30);
+            Maneger M2 = new Maneger("serega", "sergeev", 34);
+            Maneger M3 = new Maneger("niga", "abama", 34);
             Emploee E1 = new Emploee("Vasia", "vasiliyev", 20);
             Emploee E2 = new Emploee("Sveta", "puper", 22);
             Emploee E3 = new Emploee("Nina", "truper", 33);
             M1.Add(E1.GetData());
             M1.Add(E2.GetData());
+            M1.Add(M2.GetData());
+            M1.Add(M3.GetData());
 
             Console.WriteLine(M1.GetData());
-            
             Console.ReadKey();
         }
 
