4d4fd452106d65658a65de835e831c33fc6749d1
xepoman <ivan.egov@mail.ru>
2019-03-31 21:44:24 +0700

    Создал рекурсию добавления работников

--- Uroki/ConsoleApp1/Program.cs
+++ Uroki/ConsoleApp1/Program.cs
@@ -20,53 +20,13 @@ namespace ConsoleApp1
 {
     class Program
     {
-         
+
+        static public Maneger M1 = new Maneger("Igor", "ivanov", 30);
 
         static public void Main(string[] args)
         {
-            Maneger M1 = new Maneger("Igor", "ivanov", 30);
-            string exit = "";
-            while (exit != "1")
-            {
-                Console.Write("Введите имя: ");
-                string name = Console.ReadLine();
 
-                Console.Write("Введите фамилию: ");
-                string surname = Console.ReadLine();
-
-                Console.Write("Введите возраст: ");
-                int age = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Введите должность М / Е: ");
-                string dolgnost = Console.ReadLine();
-                if(dolgnost == "М")
-                {
-                    Maneger M2 = new Maneger(name, surname, age);
-                    M1.Add(M2);
-
-                    Console.Write("Введите работника М2 имя: ");
-                    name = Console.ReadLine();
-
-                    Console.Write("Введите работника М2 фамилию: ");
-                    surname = Console.ReadLine();
-
-                    Console.Write("Введите работника М2 возраст: ");
-                    age = Convert.ToInt32(Console.ReadLine());
-
-                    Emploee E2 = new Emploee(name, surname, age);
-                    M2.Add(E2);
-                }
-                else if(dolgnost == "Е")
-                {
-                    Emploee E1 = new Emploee(name, surname, age);
-                    M1.Add(E1);
-                    Console.WriteLine("работник создан");
-                }
-                Console.WriteLine("Ведите 1 если хотите выйти: ");
-                exit = Console.ReadLine();
-            }
-            Console.WriteLine(M1.GetData(1));
-
+            svoistvaManeger(M1);
 
             /*  Maneger M2 = new Maneger("serega", "sergeev", 34);
               Maneger M3 = new Maneger("petya", "abama", 54);
@@ -79,9 +39,65 @@ namespace ConsoleApp1
               M1.Add(M3);
               M2.Add(E3);*/
 
-
+            Console.WriteLine(M1.GetData(0));
             Console.ReadKey();
         }
+      static public void svoistvaManeger(Maneger M)
+      {
+            
+            string name;
+            string surname;
+            int age;
+            string Exit;
+            Console.Write("Хотите выйти? Если Да нажмите Y:");
+            Exit = Console.ReadLine();
 
+            if (Exit != "Y")
+            {
+                Console.Write("Введите должность М / Е: ");
+                string dolgnost = Console.ReadLine();
+
+                if (dolgnost == "M" || dolgnost == "m")
+                {
+                    Console.Write("Введите имя менеджера: ");
+                    name = Console.ReadLine();
+
+                    Console.Write("Введите фамилию менеджкра: ");
+                    surname = Console.ReadLine();
+
+                    Console.Write("Введите возраст менеджера: ");
+                    age = Convert.ToInt32(Console.ReadLine());
+
+                    Maneger M2 = new Maneger(name, surname, age);
+                    M.Add(M2);
+                    svoistvaManeger(M2);
+
+                }
+                else if (dolgnost == "Е" || dolgnost == "e")
+                {
+                    Console.Write("Введите имя работника М1 : ");
+                    name = Console.ReadLine();
+
+                    Console.Write("Введите фамилию работника М1 : ");
+                    surname = Console.ReadLine();
+
+                    Console.Write("Введите возраст работника М1 : ");
+                    age = Convert.ToInt32(Console.ReadLine());
+
+                    Emploee E1 = new Emploee(name, surname, age);
+                    M.Add(E1);
+                    Console.WriteLine("работник создан");
+                    svoistvaManeger(M);
+                }
+
+
+            }
+            else if (Exit == "Y");
+
+
+            /*  Console.WriteLine("Ведите 1 если хотите выйти: ");
+              exit = Console.ReadLine();
+          }*/
+        }
     }
 }
